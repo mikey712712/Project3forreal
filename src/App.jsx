6a0a0f4a4aa900f58cddeaf567000354c974292a
Mikey71212 <michaelholton712@gmail.com>
2022-10-29 16:06:30 +1100

    firestore code updated for web version 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,42 @@
 import "./App.css"
+// COMPONENTS
 import Button from "./components/Button"
 import CurrentRoom from "./components/CurrentRoom"
 import Dialog from "./components/Dialog"
 import Videos from "./components/Videos"
-import createRoom from "./functions/createRoom"
-import openUserMedia from "./functions/openUserMedia"
-import joinRoom from "./functions/joinRoom"
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from "firebase/firestore"
-// import { initializeApp } from 'firebase-admin/app';
-// import { db } from './initFire';
-// import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore"
 
-// import serviceAccount from "./keys.json"
-// import "firebase/firestore"
+// FUNCTION/LIBRARY IMPORTS
+import { createRoom } from "./functions/FirebaseRTC1"
+import { openUserMedia } from "./functions/FirebaseRTC1"
+import { joinRoom } from "./functions/FirebaseRTC1"
+import { hangUp } from "./functions/FirebaseRTC1"
+import { init } from "./functions/FirebaseRTC1"
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
 const firebaseConfig = {
-  ***REMOVED***
-  ***REMOVED***
-  ***REMOVED***
-  ***REMOVED***
-  ***REMOVED***
-  ***REMOVED***
+	***REMOVED***
+	***REMOVED***
+	***REMOVED***
+	***REMOVED***
+	***REMOVED***
+	***REMOVED***
 }
+
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 function App() {
+	init()
 	return (
 		<div className="App">
 			<h1>Welcome to Bruh moment!</h1>
 			<Button btnId={"cameraBtn"} iElement={"perm_camera_mic"} spanText={"Open camera & microphone"} btnOnClick={openUserMedia} />
 			<Button btnId={"createBtn"} iElement={"group_add"} spanText={"Create room"} btnOnClick={createRoom} />
 			<Button btnId={"joinBtn"} iElement={"group"} spanText={"Join room"} btnOnClick={joinRoom} />
-			<Button btnId={"hangupBtn"} iElement={"close"} spanText={"Hangup"} />
+			<Button btnId={"hangupBtn"} iElement={"close"} spanText={"Hangup"} btnOnClick={hangUp} />
 			<CurrentRoom />
 			<Videos />
 			<Dialog />
-			<script src="./components/app.js"></script>
 		</div>
 	)
 }
