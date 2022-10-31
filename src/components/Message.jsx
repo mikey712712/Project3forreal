d7e4348065bff2d499ed050a80747d638035253a
dyldyl <dylanfitzmaurice@gmail.com>
2022-10-31 16:23:19 +1100

    contacts attempt

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,7 +4,7 @@ export default function Message({ message, author, mine }) {
 	if (mine === "yes") {
 		return (
 			<Flex w="100%" justify="flex-end">
-				<Flex borderRadius="5px" backgroundColor="#1B4965" color="white" minW="100px" maxW="350px" my="1" p="3">
+				<Flex borderRadius="5px" backgroundColor="#1B4965" m="5px" color="white" minW="100px" maxW="350px" my="1" p="3">
 					<Text>{message}</Text>
 				</Flex>
 			</Flex>
@@ -12,7 +12,7 @@ export default function Message({ message, author, mine }) {
 	} else {
 		return (
 			<Flex w="100%">
-				<Flex borderRadius="5px" backgroundColor="#BEE9E8" color="black" minW="100px" maxW="350px" my="1" p="3">
+				<Flex borderRadius="5px" backgroundColor="#BEE9E8" m="5px" color="black" minW="100px" maxW="350px" my="1" p="3">
 					<Text>{message}</Text>
 				</Flex>
 			</Flex>
