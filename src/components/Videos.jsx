2aad7b368f013d16631b57b9c1145b56176648a1
Mikey71212 <michaelholton712@gmail.com>
2022-10-31 11:17:55 +1100

    Styling + Routing

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,14 @@
+import { Box, Flex } from "@chakra-ui/react"
+
 export default function Videos() {
 	return (
-		<div id="videos">
-			<video id="localVideo" muted autoPlay playsInline></video>
-			<video id="remoteVideo" autoPlay playsInline></video>
-		</div>
+		<Flex id="videos" w="80%" m="0 auto" boxSizing="border-box" p="20px" gap="20px" justifyContent="center">
+			<Box>
+				<video id="localVideo" muted autoPlay playsInline></video>
+				<Box position="relative">
+					<video id="remoteVideo" autoPlay playsInline></video>
+				</Box>
+			</Box>
+		</Flex>
 	)
 }
