9b916c9b8120721c4cd45c0640de671a46c49b03
Mikey71212 <michaelholton712@gmail.com>
2022-11-01 14:53:05 +1100

    changed components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,24 @@
 import Contacts from "./Contacts"
 import { Routes, Route } from "react-router-dom"
-import VideoPage from "./VideoPage"
 import Login from "./Login"
 import Register from "./Register"
 import UserSettings from "./UserSettings"
 import Account from "./Account"
 import Videos from "./Videos"
 import UserList from "./UserList"
+import ChatPage from "./ChatPage"
+import Home from "./Home"
 import { useState } from "react"
 import { Box } from "@chakra-ui/react"
 
 export default function Main({ user }) {
-	const [videoOn, setVideoOn] = useState(false)
+	const [videoOn, setVideoOn] = useState(null)
 	return (
 		<Box>
 			<Contacts user={user} />
 			<Routes>
-				<Route path="/" element={<VideoPage setVideoOn={setVideoOn} />} />
+				<Route path="/" element={<Home user={user} />} />
+				<Route path="/chat" element={<ChatPage setVideoOn={setVideoOn} />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/userSettings" element={<UserSettings />} />
@@ -25,7 +27,7 @@ export default function Main({ user }) {
 			</Routes>
 			<>
 				{videoOn ? (
-					<Box position="fixed" w="100%" h="100%" top="36px" left="0">
+					<Box position="fixed" w={"100%"} h="100%" top="36px" left="0">
 						<Videos setVideoOn={setVideoOn} />
 					</Box>
 				) : null}
@@ -33,3 +35,9 @@ export default function Main({ user }) {
 		</Box>
 	)
 }
+
+// : videoMode === "small" ? (
+//     <Box position="fixed" w="100%" h="100%" top="36px" left="0">
+//         <Videos setVideoOn={setVideoOn} />
+//     </Box>
+// )
