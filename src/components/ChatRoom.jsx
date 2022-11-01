570dc0e8a1a65ad16d5e631fc58bc4102bf7f54d
Mikey71212 <michaelholton712@gmail.com>
2022-11-01 12:41:59 +1100

    app wide video component

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -3,17 +3,11 @@ import MessageList from "./MessageList"
 import ChatRoomHeader from "./ChatRoomHeader"
 import VideoButton from "./VideoButton"
 import { Box, Flex } from "@chakra-ui/react"
-import Dialog from "./Dialog"
-import { createRoom } from "../functions/FirebaseRTC"
 import { openUserMedia } from "../functions/FirebaseRTC"
-import { joinRoom } from "../functions/FirebaseRTC"
-import { hangUp } from "../functions/FirebaseRTC"
-import { useState } from "react"
 
-export default function ChatRoom({ roomNumber, videoOn, setVideoOn }) {
-	const [joinMode, setJoinMode] = useState(false)
+export default function ChatRoom({ roomNumber, setVideoOn }) {
 	return (
-		<Box w="60%" m="0 auto" border="1px solid black" className="chatroom">
+		<Box w="60%" m="0 auto" border="1px solid black" className="chatroom" zIndex="0">
 			<ChatRoomHeader roomNumber={roomNumber} />
 			<MessageList roomNumber={roomNumber} />
 			<Flex p="5px" justifyContent="center">
@@ -27,21 +21,6 @@ export default function ChatRoom({ roomNumber, videoOn, setVideoOn }) {
 						openUserMedia()
 					}}
 				/>
-				<VideoButton btnId={"createBtn"} iElement={"group_add"} spanText={"Create room"} btnOnClick={createRoom} />
-				{joinMode ? (
-					<Dialog />
-				) : (
-					<VideoButton
-						btnId={"joinBtn"}
-						iElement={"group"}
-						spanText={"Join room"}
-						btnOnClick={() => {
-							setJoinMode(true)
-							setTimeout(joinRoom())
-						}}
-					/>
-				)}
-				<VideoButton btnId={"hangupBtn"} iElement={"close"} spanText={"Hangup"} btnOnClick={hangUp} />
 			</Flex>
 		</Box>
 	)
