8d5b66a2396b030324aafe229cc6a282ea40a87c
Mikey71212 <michaelholton712@gmail.com>
2022-11-01 15:40:59 +1100

    fullscreen chat option

--- src/components/VideoButton.jsx
+++ src/components/VideoButton.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/react"
 export default function VideoButton({ btnId, btnOnClick, spanText }) {
 	return (
-		<Button h="30px" m="0 2px" color="white" backgroundColor="#1B4965" onClick={btnOnClick} id={btnId}>
+		<Button w="fit-content" h="30px" m="0 2px" color="white" backgroundColor="#1B4965" onClick={btnOnClick} id={btnId}>
 			{spanText}
 		</Button>
 	)
