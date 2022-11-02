ad772f8640b163ab2626ee014806601d4c76fa1a
Mikey71212 <michaelholton712@gmail.com>
2022-11-02 14:09:23 +1100

    create room id

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react"
 
 export default function ChatPage({ setVideoOn, roomNumber }) {
 	return (
-		<Box position="relative" h="100%" w="100%" m="0 auto">
+		<Box bgColor="unset" position="relative" h="100%" w="100%" m="0 auto">
 			<ChatRoom setVideoOn={setVideoOn} roomNumber={roomNumber} />
 		</Box>
 	)
