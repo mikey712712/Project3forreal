70d6aa96b162c6c2e53f24eed39da8e0b2a1af90
Mikey71212 <michaelholton712@gmail.com>
2022-11-02 16:17:03 +1100

    calling fully functional

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -1,7 +1,7 @@
 import { Button, Text, Flex } from "@chakra-ui/react"
 import { joinRoomById, openUserMedia } from "../functions/FirebaseRTC"
 
-export default function Call({ incomingCall, setVideoOn }) {
+export default function Call({ setIncomingCall, incomingCall, setVideoOn }) {
 	return (
 		<Flex zIndex="200" position="absolute" top="50%" left="50%">
 			<Text fontSize={"5em"}>You are getting a call</Text>
@@ -10,6 +10,7 @@ export default function Call({ incomingCall, setVideoOn }) {
 					setVideoOn("full")
 					await openUserMedia()
 					await joinRoomById(incomingCall)
+					setIncomingCall("joined")
 				}}
 			>
 				Accept
