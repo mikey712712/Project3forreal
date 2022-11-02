5b6b88708e7a391640b1c83c56002f6950c14923
Mikey71212 <michaelholton712@gmail.com>
2022-11-02 15:06:41 +1100

    incoming call button

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -1,5 +1,20 @@
-import { Text } from "@chakra-ui/react"
+import { Button, Text } from "@chakra-ui/react"
+import { joinRoomById, openUserMedia } from "../functions/FirebaseRTC"
 
-export default function Call() {
-	return <Text fontSize={"5em"}>You are getting a call</Text>
+export default function Call({ roomNumber }) {
+	return (
+		<Flex>
+			<Text zIndex="200" position="absolute" top="50%" left="50%" fontSize={"5em"}>
+				You are getting a call
+			</Text>
+			<Button
+				onClick={() => {
+					openUserMedia()
+					joinRoomById(roomNumber)
+				}}
+			>
+				Accept
+			</Button>
+		</Flex>
+	)
 }
