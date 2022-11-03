cfd13ccefc76191a4bb58e4f73d84329ab6c69d7
Mikey71212 <michaelholton712@gmail.com>
2022-11-03 20:31:55 +1100

    login + register reroute if already logged in

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import {
 	Flex,
 	Heading,
@@ -9,7 +9,6 @@ import {
 	InputLeftElement,
 	chakra,
 	Box,
-	Avatar,
 	FormControl,
 	FormHelperText,
 	InputRightElement,
@@ -17,90 +16,97 @@ import {
 import { FaUserAlt, FaLock } from "react-icons/fa"
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
 import { createUserRealTimeDB } from "../functions/FirebaseRTDB"
-import { auth, RealTimeDB } from "../App"
-import { Navigate, useNavigate, Link } from "react-router-dom"
+import { auth } from "../App"
+import { useNavigate, Link } from "react-router-dom"
 const CFaUserAlt = chakra(FaUserAlt)
 const CFaLock = chakra(FaLock)
-export default function Register() {
+export default function Register({ user }) {
 	const navigate = useNavigate()
+	useEffect(() => {
+		if (user) {
+			navigate("/")
+		}
+	}, [user])
 	const [showPassword, setShowPassword] = useState(false)
 	const [formValue, setFormValue] = useState({})
 	const [fields, setFields] = useState({})
-	const handleShowClick = () => setShowPassword(!showPassword)
-	const onType = (event) => {
-		const { name, value } = event.target
-		setFields({ ...fields, [name]: value })
-	}
-	const onSignup = async (event) => {
-		event.preventDefault()
-		setFormValue(fields)
-		await createUserWithEmailAndPassword(auth, fields.email, fields.password)
-			.then((userCredential) => {
-				// Signed in
+	if (!user) {
+		const handleShowClick = () => setShowPassword(!showPassword)
+		const onType = (event) => {
+			const { name, value } = event.target
+			setFields({ ...fields, [name]: value })
+		}
+		const onSignup = async (event) => {
+			event.preventDefault()
+			setFormValue(fields)
+			await createUserWithEmailAndPassword(auth, fields.email, fields.password)
+				.then((userCredential) => {
+					// Signed in
 
-				const user = userCredential.user
-				// create record in RTDB
-				createUserRealTimeDB(user.uid, fields.displayName, fields.email)
+					const user = userCredential.user
+					// create record in RTDB
+					createUserRealTimeDB(user.uid, fields.displayName, fields.email)
+				})
+				.catch((error) => {
+					const errorCode = error.code
+					const errorMessage = error.message
+					console.log(errorCode, errorMessage)
+				})
+			await updateProfile(auth.currentUser, {
+				displayName: fields.displayName,
 			})
-			.catch((error) => {
-				const errorCode = error.code
-				const errorMessage = error.message
-				console.log(errorCode, errorMessage)
-			})
-		await updateProfile(auth.currentUser, {
-			displayName: fields.displayName,
-		})
-		navigate("/account")
-	}
+			navigate("/account")
+		}
 
-	return (
-		<Flex flexDirection="column" width="100wh" height="100vh" backgroundColor="gray.200" justifyContent="center" alignItems="center">
-			<Stack flexDir="column" mb="2" justifyContent="center" alignItems="center">
-				<Heading fontSize="1.7em" color="#0083a3">
-					Register a QuickChat account
-				</Heading>
-				<Box minW={{ base: "90%", md: "468px" }}>
-					<form onSubmit={onSignup}>
-						<Stack spacing={4} p="1rem" backgroundColor="whiteAlpha.900" boxShadow="md">
-							<FormControl>
-								<InputGroup>
-									<InputLeftElement pointerEvents="none" children={<CFaUserAlt color="gray.300" />} />
-									<Input type="email" name="email" placeholder="email address" onChange={onType} />
-								</InputGroup>
-							</FormControl>
-							<FormControl>
-								<InputGroup>
-									<InputLeftElement pointerEvents="none" children={<CFaUserAlt color="gray.300" />} />
-									<Input type="text" name="displayName" placeholder="display name" onChange={onType} />
-								</InputGroup>
-							</FormControl>
-							<FormControl>
-								<InputGroup>
-									<InputLeftElement pointerEvents="none" color="gray.300" children={<CFaLock color="gray.300" />} />
-									<Input type={showPassword ? "text" : "password"} name="password" placeholder="Password" onChange={onType} />
-									<InputRightElement width="4.5rem">
-										<Button h="1.75rem" size="sm" onClick={handleShowClick}>
-											{showPassword ? "Hide" : "Show"}
-										</Button>
-									</InputRightElement>
-								</InputGroup>
-								<FormHelperText textAlign="right">
-									<Link>forgot password?</Link>
-								</FormHelperText>
-							</FormControl>
-							<Button borderRadius={0} color="white" type="submit" variant="solid" bgColor="#0083a3" width="full">
-								Sign Up
-							</Button>
-						</Stack>
-					</form>
+		return (
+			<Flex flexDirection="column" width="100wh" height="100vh" backgroundColor="gray.200" justifyContent="center" alignItems="center">
+				<Stack flexDir="column" mb="2" justifyContent="center" alignItems="center">
+					<Heading fontSize="1.7em" color="#0083a3">
+						Register a QuickChat account
+					</Heading>
+					<Box minW={{ base: "90%", md: "468px" }}>
+						<form onSubmit={onSignup}>
+							<Stack spacing={4} p="1rem" backgroundColor="whiteAlpha.900" boxShadow="md">
+								<FormControl>
+									<InputGroup>
+										<InputLeftElement pointerEvents="none" children={<CFaUserAlt color="gray.300" />} />
+										<Input type="email" name="email" placeholder="email address" onChange={onType} />
+									</InputGroup>
+								</FormControl>
+								<FormControl>
+									<InputGroup>
+										<InputLeftElement pointerEvents="none" children={<CFaUserAlt color="gray.300" />} />
+										<Input type="text" name="displayName" placeholder="display name" onChange={onType} />
+									</InputGroup>
+								</FormControl>
+								<FormControl>
+									<InputGroup>
+										<InputLeftElement pointerEvents="none" color="gray.300" children={<CFaLock color="gray.300" />} />
+										<Input type={showPassword ? "text" : "password"} name="password" placeholder="Password" onChange={onType} />
+										<InputRightElement width="4.5rem">
+											<Button h="1.75rem" size="sm" onClick={handleShowClick}>
+												{showPassword ? "Hide" : "Show"}
+											</Button>
+										</InputRightElement>
+									</InputGroup>
+									<FormHelperText textAlign="right">
+										<Link>forgot password?</Link>
+									</FormHelperText>
+								</FormControl>
+								<Button borderRadius={0} color="white" type="submit" variant="solid" bgColor="#0083a3" width="full">
+									Sign Up
+								</Button>
+							</Stack>
+						</form>
+					</Box>
+				</Stack>
+				<Box>
+					Already have an account?{" "}
+					<Link to="/login" color="teal.500" href="#">
+						Log In
+					</Link>
 				</Box>
-			</Stack>
-			<Box>
-				Already have an account?{" "}
-				<Link to="/login" color="teal.500" href="#">
-					Log In
-				</Link>
-			</Box>
-		</Flex>
-	)
+			</Flex>
+		)
+	}
 }
