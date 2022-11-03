aefc8b182a7f69188bd9055afb876cda6b5979ce
Mikey71212 <michaelholton712@gmail.com>
2022-11-04 10:51:33 +1100

    firebaseconfig fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css"
 // FUNCTION/LIBRARY IMPORTS
+// import * as dotenv from "dotenv"
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 import { getDatabase, set, ref, onDisconnect } from "firebase/database"
@@ -8,19 +9,19 @@ import { BrowserRouter } from "react-router-dom"
 import { getStorage } from "firebase/storage"
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { useEffect, useState } from "react"
-
+// dotenv.config()
+// console.log(typeof process.env.REACT_APP_APIKEY) // object { BASIC : 'basic' }
 import Main from "./components/Main"
-const firebaseConfig = {
-	***REMOVED***
-	***REMOVED***
-	***REMOVED***
-	***REMOVED***
-	***REMOVED***
-	***REMOVED***
-	***REMOVED***
-}
-
-const firebaseApp = initializeApp(firebaseConfig)
+const firebaseApp = initializeApp({
+	apiKey: import.meta.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+	databaseURL: import.meta.env.VITE_DATABASEURL,
+	appId: import.meta.env.VITE_APPID,
+})
+console.log(import.meta.env)
 export const db = getFirestore(firebaseApp)
 export const RealTimeDB = getDatabase(firebaseApp)
 export const auth = getAuth(firebaseApp)
