5e5edd4929120cd0257d273f942b6b4978082459
Mikey71212 <michaelholton712@gmail.com>
2022-11-03 12:53:37 +1100

    photoURLs working

--- src/functions/FirebaseStorage.js
+++ src/functions/FirebaseStorage.js
@@ -1,17 +1,21 @@
-import { storage } from "../App"
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import { auth, RealTimeDB, storage } from "../App"
+import { uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import { updateProfile } from "firebase/auth"
+import { onValue, set, ref } from "firebase/database"
+import { ref as storeRef } from "firebase/storage"
 
 export function updateProfilePicture(uid, file) {
-	const storageRef = ref(storage, "profilePictures/" + uid) // create a new ref / path for the image
+	const storageRef = storeRef(storage, "profilePictures/" + uid) // create a new ref / path for the image
 	const metadata = {
 		contentType: "image/jpeg",
 	}
-
+	console.log(file)
 	const uploadTask = uploadBytesResumable(storageRef, file, metadata)
 
 	uploadTask.on(
 		"state_changed",
 		(snapshot) => {
+			console.log(snapshot)
 			// Observe state change events such as progress, pause, and resume
 			// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
 			const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
@@ -27,12 +31,24 @@ export function updateProfilePicture(uid, file) {
 		},
 		(error) => {
 			// Handle unsuccessful uploads
+			console.log(error.message)
 		},
 		() => {
 			// Handle successful uploads on complete
 			// For instance, get the download URL: https://firebasestorage.googleapis.com/...
-			getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-				console.log("File available at", downloadURL)
+			getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+				await updateProfile(auth.currentUser, {
+					photoURL: downloadURL,
+				})
+				const userRef = ref(RealTimeDB, `Users/${auth.currentUser.uid}`)
+				onValue(
+					userRef,
+					async (snapshot) => {
+						const data = snapshot.val()
+						await set(ref(RealTimeDB, `Users/${auth.currentUser.uid}/${Object.keys(data)[0]}/photoURL`), downloadURL)
+					},
+					{ onlyOnce: true }
+				)
 			})
 		}
 	)
